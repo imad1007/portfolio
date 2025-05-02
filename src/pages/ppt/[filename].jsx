63ea106ad83ea3ff29ad1d0b7704ce6a2110a00a
imad <imadhamdouch2@gmail.com>
2025-05-02 21:48:45 +0100

    V3.0

---
+++ src/pages/ppt/[filename].jsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+
+const PowerPointViewer = () => {
+  const router = useRouter();
+  const { filename } = router.query;
+
+  const fileUrl = `http://portfolio-six-gules-85.vercel.app/ppt/${filename}.ppt`; // or .pptx
+  const viewerUrl = `https://docs.google.com/gview?url=${fileUrl}&embedded=true`;
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-screen">
+        {filename ? (
+          <iframe
+            src={viewerUrl}
+            className="w-full h-screen border rounded-lg shadow-lg"
+            title="PPT Viewer"
+          />
+        ) : (
+          <p className="text-center text-gray-500">Loading presentation...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PowerPointViewer;
